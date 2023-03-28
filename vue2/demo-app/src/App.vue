98ccf69fb58d2ba00a835c405c560c31ba1c85ee
keito-isurugi <ja224kg@gmail.com>
2023-03-28 10:26:11 +0900

    vue router, vuetify導入

--- vue2/demo-app/src/App.vue
+++ vue2/demo-app/src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <h1>Vue2 DEMO</h1>
+      </div>
+      <v-spacer></v-spacer>
+      <v-hover v-slot="{ hover }">
+        <router-link 
+          to="/" 
+          class="white--text font-weight-bold"
+          :style="{ 'text-decoration': hover ? 'underline' : 'none' }"
+          >HOME</router-link>
+      </v-hover>
+      <v-hover v-slot="{ hover }">
+        <router-link 
+          to="./about" 
+          class="white--text font-weight-bold"
+          :style="{ 'text-decoration': hover ? 'underline' : 'none' }"
+          >ABOUT</router-link>
+      </v-hover>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+  import Vue from 'vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default Vue.extend({
+    name: 'App',
+
+    data: () => ({
+      //
+    }),
+  });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
