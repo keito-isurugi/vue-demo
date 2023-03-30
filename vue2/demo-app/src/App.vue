9e1f72aa36a8c1ab4d223d070c7d05b9e3e593ff
keito-isurugi <ja224kg@gmail.com>
2023-03-30 17:39:46 +0900

    vuetify サイドメニュー

--- vue2/demo-app/src/App.vue
+++ vue2/demo-app/src/App.vue
@@ -5,25 +5,39 @@
       color="primary"
       dark
     >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
       <div class="d-flex align-center">
         <h1>Vue2 DEMO</h1>
       </div>
       <v-spacer></v-spacer>
-      <v-hover v-slot="{ hover }">
-        <router-link 
-          to="/" 
-          class="white--text font-weight-bold"
-          :style="{ 'text-decoration': hover ? 'underline' : 'none' }"
-          >HOME</router-link>
-      </v-hover>
-      <v-hover v-slot="{ hover }">
-        <router-link 
-          to="./about" 
-          class="white--text font-weight-bold"
-          :style="{ 'text-decoration': hover ? 'underline' : 'none' }"
-          >ABOUT</router-link>
-      </v-hover>
     </v-app-bar>
+
+    <v-navigation-drawer
+        v-model="drawer"
+        fixed
+        temporary
+      >
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item-group>
+            <v-list-item
+              v-for="(menuItem, index) in menuItems"
+              :key="index"
+              class="v-list-item"
+              >
+              <v-hover v-slot="{ hover }">
+                <router-link 
+                  :to="menuItem.url"
+                  class="link"
+                  :style="{ 'text-decoration': hover ? 'underline' : 'none' }"
+                  >{{ menuItem.name }}</router-link>
+              </v-hover>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
 
     <v-main>
       <router-view/>
@@ -33,12 +47,28 @@
 
 <script lang="ts">
   import Vue from 'vue';
+  import constants from './common/constants'
 
   export default Vue.extend({
     name: 'App',
 
     data: () => ({
-      //
+      drawer: false,
+      menuItems: constants.menuItems
     }),
   });
 </script>
+
+<style scoped>
+  .v-list-item {
+    height: 40px;
+    width: 100%;
+  }
+  .link {
+    height: 100%;
+    width: 100%;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+  }
+</style>
